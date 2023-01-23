d638d9ab1e005ba2cd240dc9ba9a36d9823d2249
Hemant.Y <Hemant.Y@jumeirah.com>
2023-01-23 19:11:00 +0530

    Pokemon App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useGetPokemonByNameQuery } from './services/pokemon';
 
 function App() {
+  const {data, error, isLoading} = useGetPokemonByNameQuery('bulbasaur')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error?(
+        <>
+        There is an error
+        </>
+      ): isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+        <h3>{data.species.name}</h3>
+        <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null
+      }
     </div>
   );
 }
